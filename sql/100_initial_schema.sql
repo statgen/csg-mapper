510a198976a28157081720e5e85f4ba16b8d9961
Chris Scheller <schelcj@pobox.com>
2015-12-09 10:17:09 -0500

    deploying to a version of mysql that does not support CURRENT_TIMESTAMP on DATETIME columns

--- sql/100_initial_schema.sql
+++ sql/100_initial_schema.sql
@@ -5,170 +5,169 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema csg_mapper
+-- Schema csgmapper
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `csg_mapper` ;
+DROP SCHEMA IF EXISTS `csgmapper` ;
 
 -- -----------------------------------------------------
--- Schema csg_mapper
+-- Schema csgmapper
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `csg_mapper` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `csg_mapper` ;
+CREATE SCHEMA IF NOT EXISTS `csgmapper` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `csgmapper` ;
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`centers`
+-- Table `csgmapper`.`centers`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`centers` ;
+DROP TABLE IF EXISTS `csgmapper`.`centers` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`centers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `csgmapper`.`centers` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NOT NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`studies`
+-- Table `csgmapper`.`studies`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`studies` ;
+DROP TABLE IF EXISTS `csgmapper`.`studies` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`studies` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `csgmapper`.`studies` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NOT NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`hosts`
+-- Table `csgmapper`.`hosts`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`hosts` ;
+DROP TABLE IF EXISTS `csgmapper`.`hosts` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`hosts` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `csgmapper`.`hosts` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NOT NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`pis`
+-- Table `csgmapper`.`pis`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`pis` ;
+DROP TABLE IF EXISTS `csgmapper`.`pis` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`pis` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `csgmapper`.`pis` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NOT NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`projects`
+-- Table `csgmapper`.`projects`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`projects` ;
+DROP TABLE IF EXISTS `csgmapper`.`projects` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`projects` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `csgmapper`.`projects` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`samples`
+-- Table `csgmapper`.`samples`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`samples` ;
+DROP TABLE IF EXISTS `csgmapper`.`samples` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`samples` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `sample_id` VARCHAR(45) NOT NULL,
-  `center_id` INT NOT NULL,
-  `study_id` INT NOT NULL,
-  `pi_id` INT NOT NULL,
-  `host_id` INT NOT NULL,
-  `project_id` INT NOT NULL,
-  `filename` VARCHAR(45) NOT NULL,
-  `run_dir` VARCHAR(45) NOT NULL,
-  `state` INT NOT NULL DEFAULT 0,
-  `ref_build` VARCHAR(45) NOT NULL DEFAULT '37',
-  `fullpath` TEXT NOT NULL,
-  `exported_at` DATETIME NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  INDEX `fk_samples_1_idx` (`center_id` ASC),
-  INDEX `fk_samples_2_idx` (`study_id` ASC),
-  INDEX `fk_samples_3_idx` (`host_id` ASC),
-  INDEX `fk_samples_4_idx` (`pi_id` ASC),
-  INDEX `fk_samples_5_idx` (`project_id` ASC),
+CREATE TABLE IF NOT EXISTS `csgmapper`.`samples` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `sample_id` VARCHAR(45) NOT NULL COMMENT '',
+  `center_id` INT NOT NULL COMMENT '',
+  `study_id` INT NOT NULL COMMENT '',
+  `pi_id` INT NOT NULL COMMENT '',
+  `host_id` INT NOT NULL COMMENT '',
+  `project_id` INT NOT NULL COMMENT '',
+  `filename` VARCHAR(45) NOT NULL COMMENT '',
+  `run_dir` VARCHAR(45) NOT NULL COMMENT '',
+  `state` INT NOT NULL DEFAULT 0 COMMENT '',
+  `ref_build` VARCHAR(45) NOT NULL DEFAULT '38' COMMENT '',
+  `fullpath` TEXT NOT NULL COMMENT '',
+  `exported_at` DATETIME NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_samples_1_idx` (`center_id` ASC)  COMMENT '',
+  INDEX `fk_samples_2_idx` (`study_id` ASC)  COMMENT '',
+  INDEX `fk_samples_3_idx` (`host_id` ASC)  COMMENT '',
+  INDEX `fk_samples_4_idx` (`pi_id` ASC)  COMMENT '',
+  INDEX `fk_samples_5_idx` (`project_id` ASC)  COMMENT '',
   CONSTRAINT `fk_samples_1`
     FOREIGN KEY (`center_id`)
-    REFERENCES `csg_mapper`.`centers` (`id`)
+    REFERENCES `csgmapper`.`centers` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_2`
     FOREIGN KEY (`study_id`)
-    REFERENCES `csg_mapper`.`studies` (`id`)
+    REFERENCES `csgmapper`.`studies` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_3`
     FOREIGN KEY (`host_id`)
-    REFERENCES `csg_mapper`.`hosts` (`id`)
+    REFERENCES `csgmapper`.`hosts` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_4`
     FOREIGN KEY (`pi_id`)
-    REFERENCES `csg_mapper`.`pis` (`id`)
+    REFERENCES `csgmapper`.`pis` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_5`
     FOREIGN KEY (`project_id`)
-    REFERENCES `csg_mapper`.`projects` (`id`)
+    REFERENCES `csgmapper`.`projects` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `csg_mapper`.`jobs`
+-- Table `csgmapper`.`jobs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `csg_mapper`.`jobs` ;
+DROP TABLE IF EXISTS `csgmapper`.`jobs` ;
 
-CREATE TABLE IF NOT EXISTS `csg_mapper`.`jobs` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `sample_id` INT(11) NOT NULL,
-  `job_id` INT NOT NULL,
-  `cluster` VARCHAR(45) NOT NULL,
-  `procs` INT NOT NULL,
-  `memory` INT(11) NOT NULL,
-  `walltime` VARCHAR(45) NOT NULL,
-  `exit_code` INT NULL,
-  `elapsed` INT(11) NULL DEFAULT 0,
-  `node` VARCHAR(45) NULL,
-  `delay` INT NULL DEFAULT 0,
-  `submitted_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `started_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `ended_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `job_id_UNIQUE` (`job_id` ASC),
-  INDEX `fk_jobs_1_idx` (`sample_id` ASC),
-  INDEX `index4` (`cluster` ASC),
+CREATE TABLE IF NOT EXISTS `csgmapper`.`jobs` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `sample_id` INT(11) NOT NULL COMMENT '',
+  `job_id` INT NOT NULL COMMENT '',
+  `cluster` VARCHAR(45) NOT NULL COMMENT '',
+  `procs` INT NOT NULL COMMENT '',
+  `memory` INT(11) NOT NULL COMMENT '',
+  `walltime` VARCHAR(45) NOT NULL COMMENT '',
+  `exit_code` INT NULL COMMENT '',
+  `elapsed` INT(11) NULL DEFAULT 0 COMMENT '',
+  `node` VARCHAR(45) NULL COMMENT '',
+  `delay` INT NULL DEFAULT 0 COMMENT '',
+  `submitted_at` DATETIME NOT NULL COMMENT '',
+  `started_at` DATETIME NULL COMMENT '',
+  `ended_at` DATETIME NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_jobs_1_idx` (`sample_id` ASC)  COMMENT '',
+  INDEX `index4` (`cluster` ASC)  COMMENT '',
   CONSTRAINT `fk_jobs_1`
     FOREIGN KEY (`sample_id`)
-    REFERENCES `csg_mapper`.`samples` (`id`)
+    REFERENCES `csgmapper`.`samples` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
