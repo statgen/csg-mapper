386dcc43f9ddaf10a3baa55bfa4eb450420e26f5
Chris Scheller <schelcj@pobox.com>
2015-12-11 10:39:05 -0500

    drop the short args for most options

--- lib/perl5/CSG/Mapper.pm
+++ lib/perl5/CSG/Mapper.pm
@@ -4,12 +4,12 @@ use App::Cmd::Setup -app;
 
 sub global_opt_spec {
   return (
-    ['debug|d',     'Debug output'],
-    ['verbose|v',   'Verbose output'],
-    ['dry-run|n',   'Dry run; show what would be done without actaully doing anything'],
-    ['help|h',      'Usage'],
-    ['cluster|c=s', 'Cluster environment (csg|flux)'],
-    ['project=s',   'Project settings to load'],
+    ['debug|d',   'Debug output'],
+    ['verbose|v', 'Verbose output'],
+    ['dry-run|n', 'Dry run; show what would be done without actaully doing anything'],
+    ['help|h',    'Usage'],
+    ['cluster=s', 'Cluster environment (valid clusters: csg|flux)'],
+    ['project=s', 'Project settings to load'],
   );
 }
 
