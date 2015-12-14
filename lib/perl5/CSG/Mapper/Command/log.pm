51c091c44c80464dd5a4c876604d49bbcc9d25bd
Chris Scheller <schelcj@pobox.com>
2015-12-14 13:41:34 -0500

    working logger class and subcommand

--- lib/perl5/CSG/Mapper/Command/log.pm
+++ lib/perl5/CSG/Mapper/Command/log.pm
@@ -4,16 +4,42 @@ use CSG::Mapper -command;
 use CSG::Base;
 use CSG::Mapper::Config;
 use CSG::Mapper::DB;
+use CSG::Mapper::Logger;
 
 sub opt_spec {
+  return (
+    ['meta-id=i', 'Job meta id (database id)'],
+    ['message=s', 'Text of message to log'],
+    ['level=s',   'Log level for this message (valid levels:debug|info|notice|warning|error|critical|alert|emergency)'],
+  );
 }
 
 sub validate_args {
   my ($self, $opts, $args) = @_;
+
+  unless ($opts->{meta_id}) {
+    $self->usage_error('Job meta id is required');
+  }
+
+  my $logger = CSG::Mapper::Logger->new(job_id => $opts->{meta_id});
+  my $level = $opts->{level} // q{info};
+
+  unless ($logger->can($level)) {
+    $self->usage_error('Invalid loglevel');
+  }
+
+  $self->{stash}->{logger} = $logger;
+  $self->{stash}->{level}  = $level;
+
 }
 
 sub execute {
   my ($self, $opts, $args) = @_;
+
+  my $logger = $self->{stash}->{logger};
+  my $level  = $self->{stash}->{level};
+
+  $logger->$level($opts->{message});
 }
 
 1;
--- lib/perl5/CSG/Mapper/Logger.pm
+++ lib/perl5/CSG/Mapper/Logger.pm
@@ -1,12 +1,11 @@
-# TODO - subclass the Screen dispatcher to add a timestamp
-#        to the log message.
+## no critic (RequireArgUnpacking, ProhibitNestedSubs, RequireFilenameMatchesPackage, ProhibitMultiplePackages)
 #
 package CSG::Mapper::Logger::Dispatch::DBI {
   use base qw(Log::Dispatch::DBI);
 
   sub create_statement {
     my $self = shift;
-    my $sql  = qq{insert into $self->{table} (job_id, level, message) values (?, ?, ?)'};
+    my $sql  = qq{insert into $self->{table} (job_id, level, message) values (?, ?, ?)};
     return $self->{dbh}->prepare($sql);
   }
 
@@ -25,25 +24,28 @@ use CSG::Mapper::Config;
 
 use Moose;
 
-has 'job_id' => (is => 'ro', isa => 'Int',                 required => 1);
-has 'log'    => (is => 'ro', isa => 'Log::Dispatch',       lazy     => 1, builder => '_build_log',);
-has '_conf'  => (is => 'ro', isa => 'CSG::Mapper::Config', lazy     => 1, builder => '_build_conf');
+has 'job_id' => (is => 'ro', isa => 'Int', required => 1);
+has '_logger'    => (is => 'rw', isa => 'Log::Dispatch', lazy => 1, builder => '_build_logger');
 
-sub _build_conf {
-  return CSG::Mapper::Config->new();
-}
-
-sub _build_log {
+sub _build_logger {
   my ($self) = @_;
 
-  my $log = Log::Dispatch->new();
+  sub _add_timestamp {
+    my (%log) = @_;
+    return sprintf '%s [%s] %s', uc($log{level}), DateTime->now(time_zone => $TIMEZONE), $log{message};
+  }
+
+  my $conf = CSG::Mapper::Config->new();
+  my $log  = Log::Dispatch->new();
 
   $log->add(
     Log::Dispatch::Screen->new(
       stdout    => 1,
       stderr    => 0,
+      newline   => 1,
       min_level => 'debug',
       max_level => 'warning',
+      callbacks => \&_add_timestamp,
     )
   );
 
@@ -51,16 +53,20 @@ sub _build_log {
     Log::Dispatch::Screen->new(
       stdout    => 0,
       stderr    => 1,
+      newline   => 1,
       min_level => 'error',
       max_level => 'emergency',
+      callbacks => \&_add_timestamp,
     )
   );
 
   $log->add(
     CSG::Mapper::Logger::Dispatch::DBI->new(
-      datasource => $self->conf->dsn,
+      datasource => $conf->dsn,
+      username   => $conf->get('db', 'user'),
+      password   => $conf->get('db', 'pass'),
       table      => 'logs',
-      min_level  => 'debug',
+      min_level  => 'info',
     )
   );
 
@@ -69,17 +75,17 @@ sub _build_log {
 
 sub _log {
   my ($self, $level, $msg) = @_;
-  return $self->log(level => $level, messages => $msg, job_id => $self->job_id);
+  return $self->_logger->log(level => $level, message => $msg, job_id => $self->job_id);
 }
 
-sub debug     {shift->_log('debug',     @_);}
-sub info      {shift->_log('info',      @_);}
-sub notice    {shift->_log('notice',    @_);}
-sub warning   {shift->_log('warning',   @_);}
-sub error     {shift->_log('info',      @_);}
-sub critical  {shift->_log('critical',  @_);}
-sub alert     {shift->_log('alert',     @_);}
-sub emergency {shift->_log('emergency', @_);}
+sub debug     {return shift->_log('debug',     @_);}
+sub info      {return shift->_log('info',      @_);}
+sub notice    {return shift->_log('notice',    @_);}
+sub warning   {return shift->_log('warning',   @_);}
+sub error     {return shift->_log('info',      @_);}
+sub critical  {return shift->_log('critical',  @_);}
+sub alert     {return shift->_log('alert',     @_);}
+sub emergency {return shift->_log('emergency', @_);}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
