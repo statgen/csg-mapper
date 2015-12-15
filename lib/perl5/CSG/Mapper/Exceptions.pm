c4d57aecda3a90e44863b7c5e98ae2b037308557
Chris Scheller <schelcj@pobox.com>
2015-12-15 10:43:07 -0500

    setup our own custom exceptions

---
+++ lib/perl5/CSG/Mapper/Exceptions.pm
@@ -0,0 +1,19 @@
+package CSG::Mapper::Exception;
+
+use Exception::Class (
+  __PACKAGE__ . '::Job::BatchFileNotFound' => {
+    description => 'Unable to locate batch file',
+  },
+  __PACKAGE__ . '::Job::BatchFileNotReadable' => {
+    description => 'Unable to read batch file',
+  },
+  __PACKAGE__ . '::Job::SubmissionFailure' => {
+    description => 'Failed to submit job with qsub',
+  },
+  __PACKAGE__ . '::Job::ProcessOutput' => {
+    description => 'Failed to parse the job submission output',
+    fields      => [qw(output)],
+  }
+);
+
+1;
