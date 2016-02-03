146ae8006a71f66b9e81b8667856e43aba96fc74
Chris Scheller <schelcj@pobox.com>
2016-02-03 15:32:11 -0500

    good enough to move on

--- lib/perl5/CSG/Mapper/Command/show.pm
+++ lib/perl5/CSG/Mapper/Command/show.pm
@@ -7,22 +7,42 @@ use CSG::Mapper::DB;
 
 sub opt_spec {
   return (
-    ['meta-id=i', 'job meta id'],
     ['info',      'display basic job info'],
+    ['meta-id=i', 'job meta id'],
     ['format=s',  'output format (yaml|txt)'],
+    ['state|s=s', 'display results for a given state [submitted|requested|failed|cancelled|completed]'],
+    ['build|b=s', 'reference build to show results for'],
   );
 }
 
 sub validate_args {
   my ($self, $opts, $args) = @_;
 
-  unless ($opts->{meta_id}) {
-    $self->usage_error('meta-id is required');
+  if ($opts->{info}) {
+    unless ($opts->{meta_id}) {
+      $self->usage_error('meta-id is required');
+    }
+
+    if ($opts->{format}) {
+      unless ($opts->{format} =~ /yaml|txt/) {
+        $self->usage_error('invalid output format');
+      }
+    }
   }
 
-  if ($opts->{format}) {
-    unless ($opts->{format} =~ /yaml|txt/) {
-      $self->usage_error('invalid output format');
+  if ($opts->{state}) {
+    my $schema = CSG::Mapper::DB->new();
+
+    unless ($schema->resultset('State')->find({name => $opts->{state}})) {
+      $self->usage_error('invalid state');
+    }
+
+    unless ($opts->{build}) {
+      $self->usage_error('build is required');
+    }
+
+    unless ($opts->{build} =~ /37|38/) {
+      $self->usage_error('invalid build');
     }
   }
 }
@@ -31,46 +51,69 @@ sub execute {
   my ($self, $opts, $args) = @_;
 
   my $schema = CSG::Mapper::DB->new();
-  my $meta   = $schema->resultset('Job')->find($opts->{meta_id});
 
   if ($opts->{info}) {
-    my $info = {
-      sample => {
-        id        => $meta->result->sample->id,
-        sample_id => $meta->result->sample->sample_id,
-        center    => $meta->result->sample->center->name,
-        study     => $meta->result->sample->study->name,
-        pi        => $meta->result->sample->pi->name,
-        host      => $meta->result->sample->host->name,
-        filename  => $meta->result->sample->filename,
-        run_dir   => $meta->result->sample->run_dir,
-        state     => $meta->result->state->name,
-        build     => $meta->result->build,
-        fullpath  => $meta->result->sample->fullpath,
-      },
-      job => {
-        id        => $meta->id,
-        job_id    => $meta->job_id,
-        cluster   => $meta->cluster,
-        procs     => $meta->procs,
-        memory    => $meta->memory,
-        walltime  => $meta->walltime,
-        node      => $meta->node,
-        delay     => $meta->delay,
-        submitted => ($meta->submitted_at) ? $meta->submitted_at->ymd . $SPACE . $meta->submitted_at->hms : $EMPTY,
-        created   => $meta->created_at->ymd . $SPACE . $meta->created_at->hms,
-      }
-    };
-
     my $format = $opts->{format} // 'yaml';
+    my $meta   = $schema->resultset('Job')->find($opts->{meta_id});
 
-    if ($format eq 'txt') {
-      print Dumper $info;
-    } else {
-      print Dump($info);
-    }
+    return $self->_info($meta, $format);
   }
 
+  if ($opts->{state}) {
+    my $results = $schema->resultset('Result')->search(
+      {
+        'me.build'   => $opts->{build},
+        'state.name' => $opts->{state},
+      },
+      {
+        join => 'state',
+      }
+    );
+
+    for my $result ($results->all()) {
+      say $result->status_line();
+    }
+  }
+}
+
+sub _info {
+  my ($self, $meta, $format) = @_;
+
+  my $info = {
+    sample => {
+      id        => $meta->result->sample->id,
+      sample_id => $meta->result->sample->sample_id,
+      center    => $meta->result->sample->center->name,
+      study     => $meta->result->sample->study->name,
+      pi        => $meta->result->sample->pi->name,
+      host      => $meta->result->sample->host->name,
+      filename  => $meta->result->sample->filename,
+      run_dir   => $meta->result->sample->run_dir,
+      state     => $meta->result->state->name,
+      build     => $meta->result->build,
+      fullpath  => $meta->result->sample->fullpath,
+    },
+    job => {
+      id        => $meta->id,
+      job_id    => $meta->job_id,
+      cluster   => $meta->cluster,
+      procs     => $meta->procs,
+      memory    => $meta->memory,
+      walltime  => $meta->walltime,
+      node      => $meta->node,
+      delay     => $meta->delay,
+      submitted => ($meta->submitted_at) ? $meta->submitted_at->ymd . $SPACE . $meta->submitted_at->hms : $EMPTY,
+      created   => $meta->created_at->ymd . $SPACE . $meta->created_at->hms,
+    }
+  };
+
+  if ($format eq 'txt') {
+    print Dumper $info;
+  } else {
+    print Dump($info);
+  }
+
+  return;
 }
 
 1;
