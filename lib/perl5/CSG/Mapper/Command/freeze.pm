b745bd465294d9addadb1156b22392d9f4d0dc40
Chris Scheller <schelcj@pobox.com>
2016-02-12 09:44:20 -0500

    new subcommand to generate the freeze exports. this may be the wrong place for that though. maybe need to let tpg's code generate these?

---
+++ lib/perl5/CSG/Mapper/Command/freeze.pm
@@ -0,0 +1,24 @@
+package CSG::Mapper::Command::freeze;
+
+use CSG::Mapper -command;
+use CSG::Base;
+use CSG::Mapper::DB;
+
+sub opt_spec {
+}
+
+sub validate_args {
+  my ($self, $opts, $args) = @_;
+}
+
+sub execute {
+  my ($self, $opts, $args) = @_;
+}
+
+1;
+
+__END__
+
+=head1
+
+CSG::Mapper::Command::freeze - cancel a remapping job
